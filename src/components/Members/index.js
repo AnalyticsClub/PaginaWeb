0c51503e8037519aeb32aa3884faaaf5cc478bac
Nicolas Bejar <74123431+AnalyticsClub@users.noreply.github.com>
2021-08-17 21:51:03 -0500

    cambios

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -6,6 +6,15 @@ import Miembro3 from '../../images/Emilio_Ceballos.png'
 import Miembro4 from '../../images/Nicolas_Bejar.png'
 import Miembro5 from '../../images/Camilo_Falla.png'
 import Miembro6 from '../../images/Nicolas_Mejia.png'
+import Miembro7 from '../../images/Federico_Wiesner.png'
+import Miembro8 from '../../images/Ma_Silva.png'
+import Miembro9 from '../../images/Daniel_Santamaria.png'
+import Miembro10 from '../../images/Carlos_Erazo.png'
+import Miembro11 from '../../images/Juan_Prieto.png'
+import Miembro12 from '../../images/Andres_Tellez.png'
+
+
+
 
 
 const Members = () => {
@@ -42,6 +51,36 @@ const Members = () => {
                     <MembersIcon src={Miembro6} />
                     <MembersH2>Nicolás Mejía</MembersH2>
                     <MembersH3>Board Member</MembersH3>
+                </MembersCard>
+                <MembersCard>
+                    <MembersIcon src={Miembro7} />
+                    <MembersH2>Federico Wiesner</MembersH2>
+                    <MembersH3>Presidente</MembersH3>
+                </MembersCard>
+                <MembersCard>
+                    <MembersIcon src={Miembro8} />
+                    <MembersH2>María A. Silva</MembersH2>
+                    <MembersH3>Vice-Presidenta</MembersH3>
+                </MembersCard>
+                <MembersCard>
+                    <MembersIcon src={Miembro9} />
+                    <MembersH2>Daniel Santamaría</MembersH2>
+                    <MembersH3>Líder de Impacto</MembersH3>
+                </MembersCard>
+                <MembersCard>
+                    <MembersIcon src={Miembro10} />
+                    <MembersH2>Carlos Erazo</MembersH2>
+                    <MembersH3>Líder de Empresas</MembersH3>
+                </MembersCard>
+                <MembersCard>
+                    <MembersIcon src={Miembro11} />
+                    <MembersH2>Juan Diego Prieto</MembersH2>
+                    <MembersH3>Co-Líder de Empresas</MembersH3>
+                </MembersCard>
+                <MembersCard>
+                    <MembersIcon src={Miembro12} />
+                    <MembersH2>Andrés F. Tellez</MembersH2>
+                    <MembersH3>Líder de Emprendimientos</MembersH3>
                 </MembersCard>
             </MembersWrapper>
         </MembersContainer>
